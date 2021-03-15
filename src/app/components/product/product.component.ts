dc2056f6fd8db51d773d2bfdb9b2673299a44cec
Mert AKKAYA <mertakkaya522@gmail.com>
2021-03-15 16:22:16 +0300

    15.03.2021

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -13,15 +13,29 @@ export class ProductComponent implements OnInit {
   products:Product[]=[]
   dataLoad=false;
 
-  constructor(private productService:ProductService) { }
+  constructor(private productService:ProductService,private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params=>{
+      if (params["categoryId"]) {
+        this.getProductsByCategory(params["categoryId"]);
+      }
+      else{
+        this.getProducts();
+      }
+    })
   }
 
   getProducts(){
     this.productService.getProducts().subscribe(response=>{
       this.products = response.data
+      this.dataLoad=true;
+    })
+  }
+
+  getProductsByCategory(categoryId:number){
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
+      this.products=response.data
       this.dataLoad=true;
     })
   }
---
+++ src/app/services/category.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  apiUrl="https://localhost:44390/api/categories/getall"
+
+  constructor(private httpClient:HttpClient) { }
+
+  getCategories():Observable<ListResponseModel<Category>>{
+   return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
+  }
+}
